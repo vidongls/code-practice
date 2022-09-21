b8179a1c8d9cc15f970f113c0f4f63e4e1827cf2
DongVi <vidongls12345@gmail.com>
2022-09-21 09:51:46 +0700

    fix fix

--- src/layouts/Header/Header.tsx
+++ src/layouts/Header/Header.tsx
@@ -6,15 +6,21 @@ const { Header: HeaderAntd } = Layout;
 
 const Header: React.FC<IHeaderProps> = (props) => {
 	return (
-		<HeaderAntd className="header-login sticky top-0">
-			<div className="container p-4 flex items-center justify-between">
+		<HeaderAntd className="header sticky top-0">
+			<div className="container flex items-center justify-between">
 				<div className="logo">
 					<img src="https://www.hackerrank.com/wp-content/uploads/2018/08/hackerrank_logo.png" alt="logo" />
 				</div>
-				<div className="navigation"></div>
+				<ul className="navigation flex items-center">
+					<li>Bài tập</li>
+					<li>Kỳ thi</li>
+					<li>Bài nộp</li>
+					<li>Xếp hạng</li>
+					<li>Live IDE</li>
+				</ul>
 				<div className="header-right flex items-center">
-					<div className="btn mr-2">Login</div>
-					<div className="btn border border-zinc-500">Sign Up</div>
+					<div className="px-4 mr-2 py-1 border border-gray-200 rounded-3xl cursor-pointer text-sm">Đăng nhập</div>
+					<div className="px-4 py-1 border border-gray-200 rounded-3xl cursor-pointer text-sm">Đăng ký</div>
 				</div>
 			</div>
 		</HeaderAntd>
--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -4,11 +4,11 @@ import DefaultLayout from "../../layouts/DefaultLayout";
 interface ILandingProps {}
 
 const Landing: React.FC<ILandingProps> = (props) => {
-	return <DefaultLayout>
-        <div className="container">
-            
-        </div>
-    </DefaultLayout>;
+	return (
+		<DefaultLayout>
+			<div className="container"></div>
+		</DefaultLayout>
+	);
 };
 
 export default Landing;
