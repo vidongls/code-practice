849e0f4c16a2eb6f63c4b2c9be400ebd10a1c2a0
DongVi <vidongls12345@gmail.com>
2022-10-12 17:23:54 +0700

    add theme for editor

---
+++ src/components/CodeEditor/defineTheme.ts
@@ -0,0 +1,63 @@
+import { loader } from '@monaco-editor/react'
+import { type } from 'os'
+
+const monacoThemes = {
+    light: 'Light',
+    'vs-dark': 'Dark',
+    active4d: 'Active4D',
+    'all-hallows-eve': 'All Hallows Eve',
+    amy: 'Amy',
+    'birds-of-paradise': 'Birds of Paradise',
+    blackboard: 'Blackboard',
+    'chrome-devtools': 'Chrome DevTools',
+    'clouds-midnight': 'Clouds Midnight',
+    'espresso-libre': 'Espresso Libre',
+    github: 'GitHub',
+    idle: 'IDLE',
+    'monokai-bright': 'Monokai Bright',
+    monokai: 'Monokai',
+    'night-owl': 'Night Owl',
+    'oceanic-next': 'Oceanic Next',
+    'pastels-on-dark': 'Pastels on Dark',
+    'slush-and-poppies': 'Slush and Poppies',
+    'solarized-dark': 'Solarized-dark',
+    'solarized-light': 'Solarized-light',
+    idlefingers: 'idleFingers',
+    monoindustrial: 'monoindustrial',
+}
+
+export type TThemes =
+    | 'light'
+    | 'vs-dark'
+    | 'active4d'
+    | 'all-hallows-eve'
+    | 'amy'
+    | 'birds-of-paradise'
+    | 'blackboard'
+    | 'chrome-devtools'
+    | 'clouds-midnight'
+    | 'espresso-libre'
+    | 'github'
+    | 'idle'
+    | 'monokai-bright'
+    | 'monokai'
+    | 'night-owl'
+    | 'oceanic-next'
+    | 'pastels-on-dark'
+    | 'slush-and-poppies'
+    | 'solarized-light'
+    | 'idlefingers'
+    | 'monoindustrial'
+
+const defineTheme = (theme: TThemes) => {
+    return new Promise((res: any) => {
+        Promise.all([loader.init(), import(`monaco-themes/themes/${monacoThemes[theme]}.json`)]).then(
+            ([monaco, themeData]) => {
+                monaco.editor.defineTheme(theme, themeData)
+                res()
+            }
+        )
+    })
+}
+
+export { defineTheme }
--- src/components/CodeEditor/index.tsx
+++ src/components/CodeEditor/index.tsx
@@ -1,22 +1,31 @@
-import React, { useState } from 'react'
-import Editor from '@monaco-editor/react'
+import React, { useEffect, useState } from 'react'
+import Editor, { Monaco } from '@monaco-editor/react'
 import { Button, Select } from 'antd'
 import { RedoOutlined } from '@ant-design/icons'
+import monacoThemes from 'monaco-themes/themes/themelist.json'
+import { defineTheme, TThemes } from './defineTheme'
 
 interface ICodeEditorProps {}
 
 const { Option } = Select
-
+const options = {
+    minimap: { enabled: false },
+}
 const CodeEditor: React.FC<ICodeEditorProps> = props => {
     const [content, setContent] = useState<string | undefined>('')
-    const [theme, setTheme] = useState('light')
+    const [theme, setTheme] = useState<TThemes>('light')
     const [language, setLanguage] = useState('javascript')
 
     const handleChangeEditor = (value: string | undefined) => {
         setContent(value)
     }
 
-    const handleChangeTheme = (value: string) => {
+    const handleChangeTheme = (value: TThemes) => {
+        if (['light', 'vs-dark'].includes(value)) {
+            setTheme(theme)
+        } else {
+            defineTheme(value).then(_ => setTheme(theme))
+        }
         setTheme(value)
     }
 
@@ -24,11 +33,15 @@ const CodeEditor: React.FC<ICodeEditorProps> = props => {
         setLanguage(value)
     }
 
+    useEffect(() => {
+        defineTheme('amy').then(_ => setTheme('amy'))
+    }, [])
+
     return (
         <div>
             <div className="flex items-center justify-between gap-3 bg-gray-50 p-5">
                 <div>
-                    <span className="mr-2">Ngôn ngữ: </span>
+                    <span className="mr-2">Theme: </span>
                     <Select
                         value={theme}
                         onChange={handleChangeTheme}
@@ -36,6 +49,14 @@ const CodeEditor: React.FC<ICodeEditorProps> = props => {
                     >
                         <Option value="light">Sáng</Option>
                         <Option value="vs-dark">Tối</Option>
+                        {Object.entries(monacoThemes).map(([key, val]) => (
+                            <Option
+                                value={key}
+                                key={key}
+                            >
+                                {val}
+                            </Option>
+                        ))}
                     </Select>
 
                     <Button
@@ -44,14 +65,14 @@ const CodeEditor: React.FC<ICodeEditorProps> = props => {
                     />
                 </div>
                 <div>
-                <span className="mr-2">Theme: </span>
+                    <span className="mr-2">Ngôn ngữ: </span>
                     <Select
                         value={language}
                         onChange={handleChangeLanguage}
                         className="w-36"
                     >
                         <Option value="javascript">Javascript</Option>
-                        <Option value="c">C</Option>
+                        <Option value="cpp">C++</Option>
                     </Select>
                 </div>
             </div>
@@ -62,6 +83,7 @@ const CodeEditor: React.FC<ICodeEditorProps> = props => {
                     language={language}
                     defaultValue={content}
                     saveViewState={false}
+                    options={options}
                     onChange={(value: string | undefined) => handleChangeEditor(value)}
                 />
             </div>
