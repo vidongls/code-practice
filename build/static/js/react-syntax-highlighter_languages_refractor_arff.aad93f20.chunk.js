0fcdbd93f55569ab7faa5065ec3a0cc6f1f4b0ca
vidongls <vidongls12345@gmail.com>
2023-01-30 13:48:39 +0700

    aad build

---
+++ build/static/js/react-syntax-highlighter_languages_refractor_arff.aad93f20.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkcode_practice=self.webpackChunkcode_practice||[]).push([[1438],{7209:function(e){function a(e){e.languages.arff={comment:/%.*/,string:{pattern:/(["'])(?:\\.|(?!\1)[^\\\r\n])*\1/,greedy:!0},keyword:/@(?:attribute|data|end|relation)\b/i,number:/\b\d+(?:\.\d+)?\b/,punctuation:/[{},]/}}e.exports=a,a.displayName="arff",a.aliases=[]}}]);
+//# sourceMappingURL=react-syntax-highlighter_languages_refractor_arff.aad93f20.chunk.js.map
---
+++ build/static/js/react-syntax-highlighter_languages_refractor_ini.f1504a6f.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkcode_practice=self.webpackChunkcode_practice||[]).push([[6495],{3409:function(t){function e(t){t.languages.ini={comment:/^[ \t]*[;#].*$/m,selector:/^[ \t]*\[.*?\]/m,constant:/^[ \t]*[^\s=]+?(?=[ \t]*=)/m,"attr-value":{pattern:/=.*/,inside:{punctuation:/^[=]/}}}}t.exports=e,e.displayName="ini",e.aliases=[]}}]);
+//# sourceMappingURL=react-syntax-highlighter_languages_refractor_ini.f1504a6f.chunk.js.map
